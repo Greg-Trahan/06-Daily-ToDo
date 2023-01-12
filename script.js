d6a4af87237ab70d1207b87bd2b4e411f8314d4a
Greg Trahan <trahan.greg@gmail.com>
2023-01-11 19:19:04 -0600

    set up API fetch

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 
 
 
-let cityName = "Athens"
-
+let cityName = "Atlanta"
+//Retrieve latitute and longitute data
 fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=e0d2f5b5ff5c2d0b45050097a2089131`)
-
 .then(function (response){
   return response.json()
 })
 
+//Retrieve weather data
 .then(function(data){
-  console.log(data)
-  let lat=data[0].lat
-  let lon=data[0].lon
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=e0d2f5b5ff5c2d0b45050097a2089131`)
+  .then (response => response.json())
+  .then(function(data){
+    console.log(data)
 
-  console.log(data[0].lat)
-  console.log(data[0].lon)
-
-  fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e0d2f5b5ff5c2d0b45050097a2089131`)
-
-  // .then (response => response.json())
-
-  // .then(function(data){
-  //   console.log(data)
-  //   let temp = data[0].temp
-  // })
+    console.log(data.list[0].main.feels_like)
+  })
 
 })
