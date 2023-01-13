af956a422fa6414514571a311a2a7eaee35fc9d4
Greg Trahan <trahan.greg@gmail.com>
2023-01-13 15:59:36 -0600

    added searchable history

--- README.md
+++ README.md
@@ -1,102 +1,24 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-## Your Task
+Name: Greg Trahan  
+Due: 14JAN2023
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+Click [here](https://greg-trahan.github.io/05-ToDoList-Assignment/) to see my final project.
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+![This is a screenshot of the final page](./ScreenGrab.PNG)
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+## Description
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+The goal of this assignment is create an hour-by-hour calendar of the day that can be updated with tasks to complete throughout the day. The tasks are stored in local storage. The current day is displayed at the top, and the current hour-block is highlighted in red. Future hour-blocks are highlighted in green.
 
-## User Story
+## Usage
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+This application can be used to list out and track tasks that need to be completed throghout the day.
 
-## Acceptance Criteria
+## What I did
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+I built a JavaScript file to display the current day and time, and allow a user to input and save text into hour-by-hour blocks of time. The time blocks are generated through a JavaScript loop, using classes, tags, and a CSS file provided to me.
 
-## Mock-Up
+## What I learned
 
-The following image shows the web application's appearance and functionality:
-
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+I learned about how day.js and moment.js can be used to track the current day and time. I learned how to store complex data types in local storage, as well as how jQuery can be used to traverse and retrieve information from the DOM.
--- script.js
+++ script.js
@@ -1,99 +1,138 @@
-
-
-const button = $('#submit-button')
-let cityName = "Minneapolis"
-
-button.on('click', function(event){
-  event.preventDefault();
-  searchCity()
-})
-
-start()
-
-function start(){
-  weather()
-  listCities()
+const button = $("#submit-button");
+let cityName = "Minneapolis";
+cityHistory = JSON.parse(localStorage.getItem("cities"));
+if (cityHistory === null) {
+  cityHistory = [];
 }
 
-function searchCity(event){
-  let textBox = $('#search-bar')
+button.on("click", function (event) {
   event.preventDefault();
-  cityName = texBox.text
-  console.log(cityName)
+  searchCity();
+});
+// button.on('click', searchCity())
+
+start();
+
+function start() {
+  weather();
+  listCities();
 }
 
-function listCities(){
-  const history = ('.city-history')
-  cityHistory = JSON.parse(localStorage.getItem('cities'))
-  if (cityHistory === null){
-    cityHistory = []
-  } else {
-    for (var i=0; i < cityHistory.length; i++){
-      history.children().append(`<li>${cityHistory[i]}</li>`)
-    }
+function searchCity() {
+  const textBox = $("#search-bar");
+  cityName = textBox.val();
+  cityHistory.push(cityName);
+  console.log(cityHistory);
+  weather();
+  saveCities();
+  listCities();
+  textBox.val("");
+}
+
+function historySearch(event) {
+  cityName = event.target.textContent;
+  weather();
+}
+
+//List previously searched cities
+function listCities() {
+  const history = $(".city-history");
+  history.children().empty();
+  for (var i = 0; i < cityHistory.length; i++) {
+    history
+      .children()
+      .append(
+        `<li class = 'li-btn' onclick='historySearch(event)'>${cityHistory[i]}</li>`
+      );
   }
 }
 
-function weather(){
+function saveCities() {
+  localStorage.setItem("cities", JSON.stringify(cityHistory));
+  // const cityHistory = $(".city-history");
+  // cityHistory.append(`<button>${cityName}</button>`);
+}
+
+function weather() {
   //Retrieve latitute and longitute data
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=e0d2f5b5ff5c2d0b45050097a2089131`)
-  .then(function (response){
-    return response.json()
-  })
-
-  //Retrieve weather data
-  .then(function(data){
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=e0d2f5b5ff5c2d0b45050097a2089131`)
-    .then (response => response.json())
-    .then(function(data){
-      
-      startWeather()
-
-      function startWeather(){
-        listCities()
-        weatherData = retrieveWeather()
-        displayWeather(weatherData)
-      }
-
-      function retrieveWeather(){
-        let i = 0
-        let weatherData = []
-        while (i < 40){
-          let day = {}
-
-          splitTime = (data.list[i].dt_txt).split(" ")
-          splitDay = splitTime[0].split("-")
-
-          day.date = splitDay[1]+"/"+splitDay[2]+"/"+splitDay[0]
-          day.temp = ((data.list[i].main.temp)-273.15).toFixed(2)
-          day.humidity = data.list[i].main.humidity
-          day.wind = data.list[i].wind.speed
-          day.clouds = data.list[i].clouds.all
-          weatherData.push(day)
-          i += 8
-        }
-
-        return weatherData
-      }
-
-      function displayWeather(weatherData){
-        let city = data.city.name
-        console.log(weatherData)
-        const currentDisplay = $('#current-forecast')
-        const fiveDayDisplay = $('.five-day')
-
-        currentDisplay.append(`<h3>${city} ${weatherData[0].date}</h3>`)
-        currentDisplay.append(`<p>Temperature: ${weatherData[0].temp} Celcius</p>`)
-        currentDisplay.append(`<p>Humidity: ${weatherData[0].humidity}%</p>`)
-        currentDisplay.append(`<p>Wind: ${weatherData[0].wind} MPH</p>`)
-
-        for (day in weatherData){
-          fiveDayDisplay.children(`#${day}`).append(`<h4>${weatherData[day].date}</h4>`)
-          fiveDayDisplay.children(`#${day}`).append(`<p>Temp: ${weatherData[day].temp} Celcius</p>`)
-          fiveDayDisplay.children(`#${day}`).append(`<p>Humidity: ${weatherData[day].humidity}%</p>`)
-          fiveDayDisplay.children(`#${day}`).append(`<p>Wind: ${weatherData[day].wind} MPH</p>`)
-        }
-      }
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=e0d2f5b5ff5c2d0b45050097a2089131`
+  )
+    .then(function (response) {
+      return response.json();
     })
-  })
+
+    //Retrieve weather data
+    .then(function (data) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=e0d2f5b5ff5c2d0b45050097a2089131`
+      )
+        .then((response) => response.json())
+        .then(function (data) {
+          startWeather();
+
+          console.log(data);
+
+          function startWeather() {
+            weatherData = retrieveWeather();
+            displayWeather(weatherData);
+          }
+
+          //Grab weather data for the next 5 days
+          function retrieveWeather() {
+            let i = 0;
+            let weatherData = [];
+            while (i < 40) {
+              let day = {};
+
+              splitTime = data.list[i].dt_txt.split(" ");
+              splitDay = splitTime[0].split("-");
+
+              day.date = splitDay[1] + "/" + splitDay[2] + "/" + splitDay[0];
+              day.temp = (data.list[i].main.temp - 273.15).toFixed(2);
+              day.humidity = data.list[i].main.humidity;
+              day.wind = data.list[i].wind.speed;
+              day.clouds = data.list[i].clouds.all;
+              weatherData.push(day);
+              i += 8;
+            }
+
+            return weatherData;
+          }
+
+          //Display the weather data on the page
+          function displayWeather(weatherData) {
+            let city = data.city.name;
+            console.log(weatherData);
+            const currentDisplay = $("#current-forecast").empty();
+            const fiveDayDisplay = $(".five-day");
+            const forecastDiv = $(".forecast");
+
+            currentDisplay.append(`<h3>${city} (${weatherData[0].date})</h3>`);
+            currentDisplay.append(
+              `<p>Temperature: ${weatherData[0].temp} Celcius</p>`
+            );
+            currentDisplay.append(
+              `<p>Humidity: ${weatherData[0].humidity}%</p>`
+            );
+            currentDisplay.append(`<p>Wind: ${weatherData[0].wind} MPH</p>`);
+
+            forecastDiv.empty();
+            for (day in weatherData) {
+              fiveDayDisplay
+                .children(`#${day}`)
+                .append(`<h4>${weatherData[day].date}</h4>`);
+              fiveDayDisplay
+                .children(`#${day}`)
+                .append(`<p>Temp: ${weatherData[day].temp} Celcius</p>`);
+              fiveDayDisplay
+                .children(`#${day}`)
+                .append(`<p>Humidity: ${weatherData[day].humidity}%</p>`);
+              fiveDayDisplay
+                .children(`#${day}`)
+                .append(`<p>Wind: ${weatherData[day].wind} MPH</p>`);
+            }
+          }
+        });
+    });
 }
